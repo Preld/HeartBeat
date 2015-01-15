db0f981e0e762beceace0db73057323f4f4d6839
Yamato Mitsui <waterpokesp@gmail.com>
2015-01-15 21:46:11 +0900

    心拍計測用にScripts及びAudioを変更
    心拍計測用に各スクリプトをArduinoに対応させた。
    Audioを適した長さのものに変更した。

---
+++ Assets/HeartBeat/Oscilloscope/WaveMaster.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class WaveMaster : MonoBehaviour
+{
+		private UtilDraw2D m_draw2D;
+		private ConnectInterface devices;
+
+		private int max_width;
+		private int max_height;
+		private int interval_width = 3;
+		private LinkedList<float> wave_point_x;
+
+		private float before_data;
+
+		private Wave[] m_wave;
+		private const int WAVE_NUM = 2;
+
+		private int separateNum;
+		private Vector3[] separateLine;
+
+		void Start ()
+		{
+				devices = GameObject.FindGameObjectWithTag ("Arduino").GetComponent<Connect2Arduino> ();
+				devices.setPort ("COM3", 115200, 1);
+				//("/dev/tty.usbmodem14121", 115200);
+				//("/dev/tty.usbmodem1451", 115200);
+				//("COM3", 115200);
+
+				m_draw2D = new UtilDraw2D ();
+				wave_point_x = new LinkedList<float> ();
+				m_wave = new Wave[WAVE_NUM];
+
+				max_width = (int)Screen.width / interval_width;
+				for (int i = 0; i < max_width; i++) {
+						wave_point_x.AddLast (i * interval_width);
+				}
+
+				max_height = (int)Screen.height;
+				float interval_height = max_height / (float)WAVE_NUM;
+				for (int i = 0; i < WAVE_NUM; i++) {
+						m_wave [i] = new Wave (interval_height, i, max_width);
+				}
+
+				// 線の数 * 先端終端
+				separateNum = WAVE_NUM - 1;
+				separateLine = new Vector3[separateNum * 2];
+				for (int i = 0; i < separateNum; i++) {
+						float height = max_height / (float)WAVE_NUM * (i + 1);
+						separateLine [i] = new Vector3 (max_width * interval_width, height, 0.0f);
+						separateLine [i + separateNum] = new Vector3 (0, height, 0.0f);
+				}
+
+		}
+
+		void Update ()
+		{
+				float[] data = devices.getVoltages ();
+				m_wave [0].addData (data [0] / 1023f);
+				float tmp = (data [0] * 0.1f + before_data * 0.9f);
+				m_wave [1].addData (tmp / 1023f);
+				before_data = tmp;
+		}
+
+		private Vector3[] posA;
+
+		void OnPostRender ()
+		{
+				// 描画開始
+				m_draw2D.Begin ();
+				{
+						// 区切り用の線を描画
+						Vector3[] Line = new Vector3[2];
+						for (int i = 0; i < separateNum; i++) {
+								Line [0] = separateLine [i];
+								Line [1] = separateLine [i + separateNum];
+								m_draw2D.DrawLines (Line, new Color (0.0f, 0.0f, 0.0f));
+						}
+
+		
+						for (int i = 0; i < WAVE_NUM; i++) {
+								posA = new Vector3[m_wave [i].wave_point.Count];
+
+								for (int j = 0; j < m_wave [i].wave_point.Count; j += 1) {
+										posA [j] = new Vector3 (wave_point_x.ElementAt (j), m_wave [i].wave_point.ElementAt (j), 0.0f);
+								}
+								if (posA.Count () >= 2)
+										m_draw2D.DrawLines (posA, new Color (0.0f, 0.0f, 0.0f));
+						}
+				}
+				// 描画終了
+				m_draw2D.End ();
+		}
+
+		public void fitButton ()
+		{
+				for (int i = 0; i < m_wave.Length; i++) {
+						m_wave [i].fitData ();
+				}
+		}
+				
+}
---
+++ Assets/HeartBeat/Scripts/HeartColor.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class HeartColor : MonoBehaviour
+{
+	//variables for calibration
+	private ConnectInterface devices;
+	private float timer = 0.0f;
+	private const float CALIBRATION_TIME = 3.5f;
+	private float[] data;
+	private float signal;
+
+	// Use this for initialization
+	void Start ()
+	{
+		devices = GameObject.FindGameObjectWithTag ("Arduino").GetComponent<Connect2Arduino> ();
+		devices.setPort ("COM3", 115200, 1);
+		//("/dev/tty.usbmodem14121", 115200);
+		//("/dev/tty.usbmodem1451", 115200);
+		//("COM3", 115200);
+		timer = CALIBRATION_TIME;
+	}
+
+	//variables
+	private float max_shot = 0.0f;
+	private float min_shot = 0.0f;
+	private float threshold_shot;
+	private float percent_shot = 0.8f;
+	private float threshold_audio;
+	private float percent_audio = 0.7f;
+	private Vector3 min_size = new Vector3 (10.0f, 10.0f, 10.0f); //通常状態Scale
+	private Vector3 max_size = new Vector3 (20.0f, 20.0f, 20.0f); //拍動状態Scale
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+		//疑似心拍 for test
+		/*
+		data = Mathf.Sin (5.0f * Time.time) * 10.0f + 10.0f;
+		Debug.Log (data);
+		threshold_shot = max_shot * percent_shot;
+		threshold_audio = max_shot * percent_audio;
+		*/
+
+		data = devices.getVoltages ();
+		signal = data [0];
+
+		if ((timer - Time.time) >= 0.0) {
+			max_shot = Mathf.Max (max_shot, signal);
+			min_shot = Mathf.Min (min_shot, signal);
+			threshold_shot = max_shot * percent_shot;
+			threshold_audio = max_shot * percent_audio;
+
+			//for test
+			/*
+			max_shot = Mathf.Max (max_shot, signal);
+			min_shot = Mathf.Min (min_shot, signal);
+			*/
+		} else {
+			//for scale
+			if (signal > threshold_shot) {
+				this.transform.localScale = max_size;
+			} else {
+				this.transform.localScale = min_size;
+			}
+
+			//for color
+			this.renderer.material.color = new Color (1, 1 - signal / max_shot, 1 - signal / max_shot);
+
+			//for light
+			this.light.intensity = (signal - min_shot) / (max_shot - min_shot) + 1.0f;
+
+			//for audio
+			if (signal > threshold_audio && !this.audio.isPlaying) {
+				this.audio.Play ();
+			}
+		}
+	}
+}
