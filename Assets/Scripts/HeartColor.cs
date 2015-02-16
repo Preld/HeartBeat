83a5aeb8d949d3a8a9523bdc2e14de3f63d235a0
Preld <programmer.called.azathoth@gmail.com>
2015-02-16 14:29:21 +0900

    Fix all files

--- Assets/Scripts/HeartColor.cs
+++ Assets/Scripts/HeartColor.cs
@@ -5,85 +5,88 @@ using UnityEngine.UI;
 
 public class HeartColor : MonoBehaviour
 {
-	//variables for calibration
-	private ConnectInterface devices;
-	private float timer = 0.0f;
-	private const float CALIBRATION_TIME = 3.5f;
+		//variables for calibration
+		private IConnect2Devices devices;
+		private float timer = 0.0f;
+		private const float CALIBRATION_TIME = 3.5f;
+		private bool _isPortOpen;
 
-	// Use this for initialization
-	void Start ()
-	{
-		devices = GameObject.FindGameObjectWithTag ("Arduino").GetComponent<Connect2Arduino> ();
-		devices.setPort ("COM3", 115200, 1);
-		/*("/dev/tty.usbmodem14121", 115200);
-		("/dev/tty.usbmodem1451", 115200);
-		("COM3", 115200);*/
-		timer = CALIBRATION_TIME;
-	}
+		// Use this for initialization
+		void Start ()
+		{
+				devices = GameObject.FindGameObjectWithTag ("Arduino").GetComponent<Connect2Arduino> ();
+				_isPortOpen = devices.SetPort ("COM3", 115200, 1);
+				/*("/dev/tty.usbmodem14121", 115200);
+				("/dev/tty.usbmodem1451", 115200);
+				("COM3", 115200);*/
+				timer = CALIBRATION_TIME;
+		}
 
-	//variables
-	private float max_shot = 0.0f;
-	private float min_shot = 10000.0f;
-	private float threshold_shot;
-	private float percent_shot = 0.7f;
-	private float threshold_audio;
-	private float percent_audio = 0.7f;
-	private Vector3 min_size = new Vector3 (10.0f, 10.0f, 10.0f); //通常状態Scale
-	private Vector3 max_size = new Vector3 (20.0f, 20.0f, 20.0f); //拍動状態Scale
-	private float[] data;
-	private float signal;
-	public Text msg;
+		//variables
+		private float max_shot = 0.0f;
+		private float min_shot = 10000.0f;
+		private float threshold_shot;
+		private float percent_shot = 0.7f;
+		private float threshold_audio;
+		private float percent_audio = 0.7f;
+		private Vector3 min_size = new Vector3 (10.0f, 10.0f, 10.0f);
+		//通常状態Scale
+		private Vector3 max_size = new Vector3 (20.0f, 20.0f, 20.0f);
+		//拍動状態Scale
+		private float[] data;
+		private float signal;
+		public Text msg;
 	
-	// Update is called once per frame
-	void Update ()
-	{
-		if (devices.getErrorMessage () != null) {
-			msg.color = new Color (1, 0, 0);
-			msg.text = "Error\n" + devices.getErrorMessage ();
-		}else{
-			data = devices.getVoltages ();
-			signal = data [0];
+		// Update is called once per frame
+		void Update ()
+		{
+				if (_isPortOpen) {
+						msg.color = new Color (1, 0, 0);
+						msg.text = "Port can not open.\n";
+				} else {
+						data = devices.GetVoltages ();
+						signal = data [0];
 	
-			//疑似心拍 for test
-			/*data = Mathf.Sin (5.0f * Time.time) * 10.0f + 10.0f;
+						//疑似心拍 for test
+						/*data = Mathf.Sin (5.0f * Time.time) * 10.0f + 10.0f;
 			threshold_shot = max_shot * percent_shot;
 			threshold_audio = max_shot * percent_audio;*/
 
-			if (timer >= 0.0) {
-			max_shot = Mathf.Max (max_shot, signal);
-			min_shot = Mathf.Min (min_shot, signal);
-			threshold_shot = max_shot * percent_shot;
-			threshold_audio = max_shot * percent_audio;
+						if (timer >= 0.0) {
+								max_shot = Mathf.Max (max_shot, signal);
+								min_shot = Mathf.Min (min_shot, signal);
+								threshold_shot = max_shot * percent_shot;
+								threshold_audio = max_shot * percent_audio;
 
-			//for test
-			/*max_shot = Mathf.Max (max_shot, signal);
+								//for test
+								/*max_shot = Mathf.Max (max_shot, signal);
 			min_shot = Mathf.Min (min_shot, signal);*/
 
-			//for text
-			msg.text = "Now Calibrating\n" + "Remaing Time : " + Mathf.CeilToInt(timer).ToString();
-			timer -= Time.deltaTime;
-			} else {
-				//for text
-				msg.text = null;
+								//for text
+								msg.text = "Now Calibrating\n" + "Remaing Time : " + Mathf.CeilToInt (timer).ToString ();
+								timer -= Time.deltaTime;
+						} else {
+								//for text
+								msg.text = null;
 
-				//for scale
-				if (signal > threshold_shot) {
-					this.transform.localScale = max_size;
-				} else {
-					this.transform.localScale = min_size;
+								//for scale
+								if (signal > threshold_shot) {
+										this.transform.localScale = max_size;
+								} else {
+										this.transform.localScale = min_size;
+								}
+
+								//for color
+								this.renderer.material.color = new Color (1, 1 - signal / max_shot, 1 - signal / max_shot);
+
+								//for light
+								this.light.intensity = (signal - min_shot) / (max_shot - min_shot) + 1.0f;
+
+								//for audio
+								if (signal > threshold_audio && !this.audio.isPlaying) {
+										this.audio.Play ();
+								}
+						}
 				}
-
-				//for color
-				this.renderer.material.color = new Color (1, 1 - signal / max_shot, 1 - signal / max_shot);
-
-				//for light
-				this.light.intensity = (signal - min_shot) / (max_shot - min_shot) + 1.0f;
-
-				//for audio
-				if (signal > threshold_audio && !this.audio.isPlaying) {
-					this.audio.Play ();
-				}
-			}
 		}
-	}
 }
--- Assets/Scripts/ParticleRed.cs
+++ Assets/Scripts/ParticleRed.cs
@@ -5,79 +5,80 @@ using UnityEngine.UI;
 
 public class ParticleRed : MonoBehaviour
 {
-	//variables for calibration
-	private ConnectInterface devices;
-	private float timer = 0.0f;
-	private const float CALIBRATION_TIME = 3.5f;
+		//variables for calibration
+		private IConnect2Devices devices;
+		private float timer = 0.0f;
+		private const float CALIBRATION_TIME = 3.5f;
+		private bool _isPortOpen;
 
-	// Use this for initialization
-	void Start () 
-	{
-		devices = GameObject.FindGameObjectWithTag ("Arduino").GetComponent<Connect2Arduino> ();
-		devices.setPort ("COM3", 115200, 1);
-		/*("/dev/tty.usbmodem14121", 115200);
+		// Use this for initialization
+		void Start ()
+		{
+				devices = GameObject.FindGameObjectWithTag ("Arduino").GetComponent<Connect2Arduino> ();
+				_isPortOpen = devices.SetPort ("COM3", 115200, 1);
+				/*("/dev/tty.usbmodem14121", 115200);
 		("/dev/tty.usbmodem1451", 115200);
 		("COM3", 115200);*/
-		timer = CALIBRATION_TIME;
-	}
+				timer = CALIBRATION_TIME;
+		}
 	
-	//variables
-	private float max_shot = 0.0f;
-	private float min_shot = 10000.0f;
-	private float threshold;
-	private float percent = 0.7f;
-	private float scale;
-	private float[] data;
-	private float signal;
-	public Text msg;
+		//variables
+		private float max_shot = 0.0f;
+		private float min_shot = 10000.0f;
+		private float threshold;
+		private float percent = 0.7f;
+		private float scale;
+		private float[] data;
+		private float signal;
+		public Text msg;
 
 
-	// Update is called once per frame
-	void Update ()
-	{
-		if (devices.getErrorMessage () != null) {
-			msg.color = new Color(1, 0, 0);
-			msg.text = "Error\n" + devices.getErrorMessage();
-		}else{
-			data = devices.getVoltages ();
-			signal = data [0];
+		// Update is called once per frame
+		void Update ()
+		{
+				if (_isPortOpen) {
+						msg.color = new Color (1, 0, 0);
+						msg.text = "Port can not open.\n";
+				} else {
+						data = devices.GetVoltages ();
+						signal = data [0];
 
-			//疑似心拍 for test
-			/*data = Mathf.Sin (5.0f * Time.time) * 10.0f + 10.0f;
+						//疑似心拍 for test
+						/*data = Mathf.Sin (5.0f * Time.time) * 10.0f + 10.0f;
 			threshold = max_shot * percent;*/
 
-			if (timer - Time.time >= 0.0) {
-				max_shot = Mathf.Max (max_shot, signal);
-				min_shot = Mathf.Min (min_shot, signal);
-				threshold = max_shot * percent;
+						if (timer - Time.time >= 0.0) {
+								max_shot = Mathf.Max (max_shot, signal);
+								min_shot = Mathf.Min (min_shot, signal);
+								threshold = max_shot * percent;
 
-				//for test
-				/*max_shot = Mathf.Max (max_shot, signal);
+								//for test
+								/*max_shot = Mathf.Max (max_shot, signal);
 				min_shot = Mathf.Min (min_shot, signal);*/
 
-				//for text
-				msg.text = "Now Calibrating\n" + "Remaining Time : " + Mathf.CeilToInt(timer).ToString();
-				timer -= Time.deltaTime;
-			}else{
-				//for text
-				msg.text = null;
+								//for text
+								msg.text = "Now Calibrating\n" + "Remaining Time : " + Mathf.CeilToInt (timer).ToString ();
+								timer -= Time.deltaTime;
+						} else {
+								//for text
+								msg.text = null;
 
-				//for scale
-				scale = (signal - min_shot) / (max_shot - min_shot) * 5.0f + 20.0f;
-				this.transform.localScale = new Vector3 (scale, scale, scale);
+								//for scale
+								scale = (signal - min_shot) / (max_shot - min_shot) * 5.0f + 20.0f;
+								this.transform.localScale = new Vector3 (scale, scale, scale);
 
-				//for light
-				this.light.intensity = (signal - min_shot) / (max_shot - min_shot) + 1.0f;
-				this.light.color = new Color(1.0f, (120.0f - signal / (max_shot - min_shot) * 50.0f) / 255.0f, (120.0f - signal / (max_shot - min_shot) * 50.0f) / 255.0f);
+								//for light
+								this.light.intensity = (signal - min_shot) / (max_shot - min_shot) + 1.0f;
+								this.light.color = new Color (1.0f, (120.0f - signal / (max_shot - min_shot) * 50.0f) / 255.0f, (120.0f - signal / (max_shot - min_shot) * 50.0f) / 255.0f);
 
-				//for particle 
-				this.particleSystem.startColor = new Color(1.0f, (100.0f - signal / (max_shot - min_shot) * 70.0f) / 255.0f, (100.0f - signal / (max_shot - min_shot) * 70.0f) / 255.0f);
+								//for particle 
+								this.particleSystem.startColor = new Color (1.0f, (100.0f - signal / (max_shot - min_shot) * 70.0f) / 255.0f, (100.0f - signal / (max_shot - min_shot) * 70.0f) / 255.0f);
 				
-				//for audio
-				if (signal > threshold && !this.audio.isPlaying) {
-					this.audio.Play ();
+								//for audio
+								if (signal > threshold && !this.audio.isPlaying) {
+										this.audio.Play ();
+								}
+						}
 				}
-			}
 		}
-	}
 }
